ec309a6ca62ec34336f320f73f98e84363075cf4
Katulondimalu <104638030+Katulondimalu@users.noreply.github.com>
2022-09-29 17:58:42 +0200

    jaja

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ const App = () => {
     if (!was_connected) {
       set_was_connected(is_connected);
     }
-    document.title = 'Patient 0';
+    document.title = 'James Bond';
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [is_connected]);
 
